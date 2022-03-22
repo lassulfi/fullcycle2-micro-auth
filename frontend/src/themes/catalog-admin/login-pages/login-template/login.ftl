6262c2053e0786405e49b120b794b2cb907bb7ba
Luis Daniel Assulfi <luis.assulfi@sensedia.com>
2022-03-21 21:57:12 -0300

    Criando link do esqueceu sua senha

--- frontend/src/themes/catalog-admin/login-pages/login-template/login.ftl
+++ frontend/src/themes/catalog-admin/login-pages/login-template/login.ftl
@@ -1,6 +1,34 @@
 <#import "layout.ftl" as layout>
 <@layout.registrationLayout title=msg("doLogIn"); section>
     <#if section = 'scripts'>
+        <script type="text/javascript">
+            var pageProps = {
+                loginEnabled: ${realm.password?string},
+                loginAction: "${url.loginAction?no_esc}",
+                <#if usernameEditDisabled?>
+                    usernameEditDisabled: true,
+                <#else>
+                    usernameEditDisabled: false,
+                <#if>
+                <#if !realm.loginWithEmailAllowed>
+                    usernameLabel: "${msg("username")}",
+                <#elseif !realm.registrationEmailAsUsername>
+                    usernameLabel: "${msg("usernameOrEmail")}",
+                <#else>
+                    usernameLabel: "${msg("email")}",
+                </#if>
+                usernameValue: "${(login.username!'')}",
+                passwordLabel: "${msg("password")}",
+                enabledRememberMe: ${realm.rememberMe?string},
+                <#if login.rememberMe??>
+                    enabledLoginRememberMe: ${login.rememberMe?string},
+                </#fi>
+                rememberMeLabel: "${msg("rememberMe")}",
+                resetPasswordAllowed: ${realm.resetPasswordAllowed?string},
+                resetPasswordUrl: "${url.loginResetCredentialsUrl}",
+                resetPasswordLabel: "${msg("doForgotPassword")}",
+            };
+        </script>
         <%= htmlWebpackPlugin.tags.bodyTags %>
     </#if>
 </@layout.registrationLayout>
--- frontend/src/themes/catalog-admin/login-pages/login-template/login.page.tsx
+++ frontend/src/themes/catalog-admin/login-pages/login-template/login.page.tsx
@@ -1,75 +1,119 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from '../../../../reportWebVitals';
-import { Box, Button, CssBaseline, Grid, Link, MuiThemeProvider, TextField } from '@material-ui/core';
+import { Box, Button, Checkbox, CssBaseline, FormControlLabel, Grid, Link, MuiThemeProvider, TextField } from '@material-ui/core';
 import theme from '../../../../theme';
 import Layout, { LayoutProps } from '../../components/Layout';
 
 declare const layoutProps: LayoutProps;
+declare const pageProps: LoginPageProps;
 
 interface LoginPageProps {
-
+    loginEnabled: boolean;
+    loginAction: string;
+    usernameEditDisabled: boolean;
+    usernameLabel: string;
+    usernameValue: string;
+    passwordLabel: string;
+    enabledRememberMe: boolean;
+    enabledLoginRememberMe?: boolean;
+    rememberMeLabel: string;
+    resetPasswordAllowed: boolean;
+    resetPasswordUrl: string;
+    resetPasswordLabel: string;
 }
 
 const LoginPage: React.FunctionComponent<LoginPageProps> = (props) => {
+    const {
+        loginEnabled,
+        loginAction,
+        usernameEditDisabled,
+        usernameLabel,
+        usernameValue,
+        passwordLabel,
+        enabledRememberMe,
+        enabledLoginRememberMe,
+        rememberMeLabel,
+        resetPasswordAllowed,
+        resetPasswordUrl,
+        resetPasswordLabel,
+    } = props;
+
     return (
         <Box padding={2}>
+            {!loginEnabled && <div>Login não habilitado. Entre em contato com o administrador</div>}
             <Grid container spacing={3} justify="space-evenly">
                 <Grid item>
-                    <TextField
-                        id="username"
-                        name="username"
-                        fullWidth
-                        variant="outlined"
-                        autoFocus
-                        autoComplete="off"
-                    />
-                    <TextField
-                        id="password"
-                        name="password"
-                        type="password"
-                        fullWidth
-                        variant="outlined"
-                        margin="normal"
-                    />
-                    <div>
-                        <div>
-                            <input
-                                type="checkbox"
-                            />
-                        </div>
-                        <Link
-                            href="#"
-                            color="secondary"
+                    <form action={loginAction} method="post">
+                        <TextField
+                            id="username"
+                            name="username"
+                            fullWidth
+                            label={usernameLabel}
+                            variant="outlined"
+                            defaultValue={usernameValue}
+                            autoFocus
+                            autoComplete="off"
+                            disabled={usernameEditDisabled}
+                        />
+                        <TextField
+                            id="password"
+                            name="password"
+                            type="password"
+                            fullWidth
+                            variant="outlined"
+                            margin="normal"
+                            autoComplete="off"
+                            label={passwordLabel}
+                        />
+                        <Grid
+                            container
+                            alignItems="center"
                         >
-
-                        </Link>
-                    </div>
-                    <Grid container>
-                        <Grid item xs={7}>
-                            <div>
-                                <Link
-                                    href="#"
-                                    color="secondary"
-                                >
-                                </Link>
-                            </div>
-                        </Grid>
-                        <Grid item xs={5}>
-                            <input
-                                type="hidden"
-                                id="id-hidden-input"
-                                name="credentialId"
-                            />
-                            <Button
-                                type="submit"
+                            {enabledRememberMe && !usernameEditDisabled && (
+                                <FormControlLabel
+                                    control={
+                                        <Checkbox
+                                            name="remeberMe"
+                                            checked={enabledLoginRememberMe}
+                                        />
+                                    }
+                                    label={rememberMeLabel}
+                                />
+                            )}
+                            {resetPasswordAllowed && (<Link
+                                href={resetPasswordUrl}
                                 color="secondary"
-                                variant="outlined"
                             >
-                                Login
-                            </Button>
+                                {resetPasswordLabel}
+                            </Link>)}
                         </Grid>
-                    </Grid>
+                        <Grid container>
+                            <Grid item xs={7}>
+                                <div>
+                                    <Link
+                                        href="#"
+                                        color="secondary"
+                                    >
+                                    </Link>
+                                </div>
+                            </Grid>
+                            <Grid item xs={5}>
+                                <input
+                                    type="hidden"
+                                    id="id-hidden-input"
+                                    name="credentialId"
+                                />
+                                <Button
+                                    type="submit"
+                                    color="secondary"
+                                    variant="outlined"
+                                >
+                                    Login
+                                </Button>
+                            </Grid>
+                        </Grid>
+                    </form>
                 </Grid>
             </Grid>
         </Box>
@@ -81,7 +125,7 @@ ReactDOM.render(
         <MuiThemeProvider theme={theme}>
             <CssBaseline />
             <Layout {...layoutProps}>
-                <LoginPage />
+                <LoginPage {...pageProps} />
             </Layout>
         </MuiThemeProvider>
     </React.StrictMode>,
